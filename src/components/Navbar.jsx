7e3d46ad253fdd0be133357569b06fe6cdbccc39
rreskey <smivasia@gmail.com>
2023-01-15 13:07:10 +0300

    Change header styling
    + Change header styling
    + Other small changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,12 @@ import React, { useEffect, useState } from 'react'
 import '../App.css'  
 import logo_wide from "../assets/logo_wide.png"
 import { useNavigate } from "react-router-dom"
-import userStateQuery from '../state/userState'
+import userStateQuery  from '../state/userState'
 import dsc from "../assets/dsc.png"
 import tw from "../assets/tw.png"
 import vk from "../assets/vk.png"
 import yt from "../assets/yt3.png"
+import unauthorized from "../assets/unauthorized.png"
 
 export default function Navbar() {
 const navigate = useNavigate();
@@ -34,21 +35,46 @@ const auth_url = 'https://osu.ppy.sh/oauth/authorize?response_type=code&client_i
 
 const [data, setData] = useState(null)
 
+useEffect(() => {
+  async function fetchData() {
+    const result = await fetch(`${window.location.origin}/api/me`)
+    const res = await result.json()
+    if (res.status === 200) {
+      setData(res)
+      return res
+  }
+  return null
+  }
+  fetchData()
+}, [])
+
+console.log(data)
+
+const unauthorized_btn = (
+  <a href={auth_url} className='login-btn'>{window.innerWidth >= 600? 'Войти' : null} <img className='unauthorized' src={unauthorized} alt='unauthorized icon'/></a>
+)
+
 return (
   <header className='inline'>
-    <img src={logo_wide} alt="logo_wide" className='logo_wide' onClick={() => navigate('/play-osu/')}/>
     <div className='nav-social-links'>
-      <a href='https://discord.com/invite/aDQhsyB' target='_blank' className='nav-social-links-btn'><img src={dsc} alt='dsc logo navbar' /></a>
+      <div className='logo_container'>
+        <img src={logo_wide} alt="logo_wide" className='logo_wide' onClick={() => navigate('/play-osu/')}/>
+      </div>
+      <a href='https://discord.com/invite/aDQhsyB' target='_blank' className='nav-social-links-btn dsc'><img src={dsc} alt='dsc logo navbar' /></a>
       <a href='https://vk.com/playosu' target='_blank' className='nav-social-links-btn'><img src={vk} alt='vk logo navbar' /></a>
       <a href='https://www.twitch.tv/playosuru' target='_blank' className='nav-social-links-btn'><img src={tw} alt='twitch logo navbar' /></a>
       <a href='https://www.youtube.com/channel/UChNf1_khGnxReYnDsU6B6uw' target='_blank' className='nav-social-links-btn'><img src={yt} alt='youtube logo navbar' /></a>
+    </div>
+    <div className='profile-btn'>
       {
-        data
-          ? <button className='profile-btn'><img className='profile-btn--img' src={data.avatar_url}/></button>
-          : <a href={auth_url} className='login-btn'>Login</a>
+      data
+        ? <a className='pfp'>{window.innerWidth >= 600? 'osuJANAIKATSURAda' : null} <img className='profile-btn--img' src={data.avatar_url}/></a>
+        : unauthorized_btn
       }
-    </div>  
+    </div>
   </header>
   )
 }
+
+// {window.innerWidth >= 600? data.username : null} 
 
